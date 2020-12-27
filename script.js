809c27a9215af37cac9383da2f2378499232d6ff
radu-kifan <67323793+radu-kifan@users.noreply.github.com>
2020-12-27 16:01:23 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+$('.me').mouseover(function() { 
+    $(this).css({"background-color": "#8B8989"});
+});
+     $('.me').mouseout(function(){
+   $(this).css({"background-color": "#483D8B"});
+  });	
+    }); 
